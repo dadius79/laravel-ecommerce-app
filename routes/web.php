7f0738b98b7b95f8f23d018df2eac3679e6db4c2
dadius79 <david.sule@outlook.com>
2021-09-22 17:32:11 +0300

    Latest ecommerce updates, category listing, product details, cart feature

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'site.pages.homepage');
 Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
+Route::get('/product/{slug}', 'Site\ProductController@show')->name('product.show');
+Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
+Route::get('/cart', 'Site\CartController@getCart')->name('checkout.cart');
+Route::get('/cart/item/{id}/remove', 'Site\CartController@removeItem')->name('checkout.cart.remove');
+Route::get('/cart/clear', 'Site\CartController@clearCart')->name('checkout.cart.clear');
 
 
 Auth::routes();
