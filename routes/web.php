00cecdc43dae46b94a2f990b6ed0328c6c7761f8
dadius79 <david.sule@outlook.com>
2021-09-24 11:43:15 +0300

    New features in order management, checkout and paypal payment processing

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,14 @@ Route::get('/cart', 'Site\CartController@getCart')->name('checkout.cart');
 Route::get('/cart/item/{id}/remove', 'Site\CartController@removeItem')->name('checkout.cart.remove');
 Route::get('/cart/clear', 'Site\CartController@clearCart')->name('checkout.cart.clear');
 
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/checkout', 'Site\CheckoutController@getCheckout')->name('checkout.index');
+    Route::post('/checkout/order', 'Site\CheckoutController@placeOrder')->name('checkout.place.order');
+
+    Route::get('checkout/payment/complete', 'Site\CheckoutController@complete')->name('checkout.payment.complete');
+
+    Route::get('account/orders', 'Site\AccountController@getOrders')->name('account.orders');
+});
 
 Auth::routes();
 require 'admin.php';
