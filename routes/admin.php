78cd5d18f93f4c9acf3207182f193cd0063a8658
dadius79 <david.sule@outlook.com>
2021-09-25 13:25:44 +0300

    Changes to Route syntax in line with laravel 8

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,12 @@
 <?php
 
-Route::group(['prefix' => 'admin'], function() {
+Route::prefix('admin')->group(function(){
 
     Route::get('login', 'App\Http\Controllers\Admin\LoginController@showLoginForm')->name('admin.auth.login');
     Route::post('login', 'App\Http\Controllers\Admin\LoginController@login')->name('admin.login.post');
     Route::get('logout', 'App\Http\Controllers\Admin\LoginController@logout')->name('admin.logout');
 
-    Route::group(['middleware' => ['auth:admin']], function () {
+    Route::middleware('auth:admin')->group(function(){
 
         Route::get('/', function(){
             return view('admin.dashboard.index');
@@ -15,7 +15,7 @@ Route::group(['prefix' => 'admin'], function() {
         Route::get('/settings', 'App\Http\Controllers\Admin\SettingController@index')->name('admin.settings');
         Route::post('/settings', 'App\Http\Controllers\Admin\SettingController@update')->name('admin.settings.update');
 
-        Route::group(['prefix' => 'categories'], function() {
+        Route::prefix('categories')->group(function() {
 
             Route::get('/', 'App\Http\Controllers\Admin\CategoryController@index')->name('admin.categories.index');
             Route::get('/create', 'App\Http\Controllers\Admin\CategoryController@create')->name('admin.categories.create');
@@ -23,9 +23,10 @@ Route::group(['prefix' => 'admin'], function() {
             Route::get('/{id}/edit', 'App\Http\Controllers\Admin\CategoryController@edit')->name('admin.categories.edit');
             Route::post('/update', 'App\Http\Controllers\Admin\CategoryController@update')->name('admin.categories.update');
             Route::get('/{id}/delete', 'App\Http\Controllers\Admin\CategoryController@delete')->name('admin.categories.delete');
+
         });
 
-        Route::group(['prefix'  =>   'attributes'], function() {
+        Route::prefix('attributes')->group(function() {
 
             Route::get('/', 'App\Http\Controllers\Admin\AttributeController@index')->name('admin.attributes.index');
             Route::get('/create', 'App\Http\Controllers\Admin\AttributeController@create')->name('admin.attributes.create');
@@ -38,10 +39,11 @@ Route::group(['prefix' => 'admin'], function() {
             Route::post('/add-values', 'App\Http\Controllers\Admin\AttributeValueController@addValues');
             Route::post('/update-values', 'App\Http\Controllers\Admin\AttributeValueController@updateValues');
             Route::post('/delete-values', 'App\Http\Controllers\Admin\AttributeValueController@deleteValues');
-        
+            
         });
 
-        Route::group(['prefix' => 'products'], function () {
+        
+        Route::prefix('products')->group(function() {
 
             Route::get('/', 'App\Http\Controllers\Admin\ProductController@index')->name('admin.products.index');
             Route::get('/create', 'App\Http\Controllers\Admin\ProductController@create')->name('admin.products.create');
@@ -62,10 +64,10 @@ Route::group(['prefix' => 'admin'], function() {
             Route::post('attributes/add', 'App\Http\Controllers\Admin\ProductAttributeController@addAttribute');
             // Delete product attribute from the current product
             Route::post('attributes/delete', 'App\Http\Controllers\Admin\ProductAttributeController@deleteAttribute');
-         
-         });
 
-        Route::group(['prefix' => 'brands'], function() {
+        });
+
+        Route::prefix('brands')->group(function(){
             Route::get('/', 'App\Http\Controllers\Admin\BrandController@index')->name('admin.brands.index');
             Route::get('/create', 'App\Http\Controllers\Admin\BrandController@create')->name('admin.brands.create');
             Route::post('/store', 'App\Http\Controllers\Admin\BrandController@store')->name('admin.brands.store');
@@ -74,10 +76,12 @@ Route::group(['prefix' => 'admin'], function() {
             Route::get('/{id}/delete', 'App\Http\Controllers\Admin\BrandController@delete')->name('admin.brands.delete');
         });
 
-        Route::group(['prefix' => 'orders'], function () {
+
+        Route::prefix('orders')->group(function(){
             Route::get('/', 'App\Http\Controllers\Admin\OrderController@index')->name('admin.orders.index');
             Route::get('/{order}/show', 'App\Http\Controllers\Admin\OrderController@show')->name('admin.orders.show');
-         });
+        });
+
     });
 
 });
