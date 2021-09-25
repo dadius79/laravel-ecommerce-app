0f3958b12dd0c9833724c3d640842f773edf3603
dadius79 <david.sule@outlook.com>
2021-09-25 13:25:44 +0300

    Changes to Route syntax in line with laravel 8

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,22 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::view('/', 'site.pages.homepage');
-Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
-Route::get('/product/{slug}', 'Site\ProductController@show')->name('product.show');
-Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
-Route::get('/cart', 'Site\CartController@getCart')->name('checkout.cart');
-Route::get('/cart/item/{id}/remove', 'Site\CartController@removeItem')->name('checkout.cart.remove');
-Route::get('/cart/clear', 'Site\CartController@clearCart')->name('checkout.cart.clear');
+Route::get('/category/{slug}', 'App\Http\Controllers\Site\CategoryController@show')->name('category.show');
+Route::get('/product/{slug}', 'App\Http\Controllers\Site\ProductController@show')->name('product.show');
+Route::post('/product/add/cart', 'App\Http\Controllers\Site\ProductController@addToCart')->name('product.add.cart');
+Route::get('/cart', 'App\Http\Controllers\Site\CartController@getCart')->name('checkout.cart');
+Route::get('/cart/item/{id}/remove', 'App\Http\Controllers\Site\CartController@removeItem')->name('checkout.cart.remove');
+Route::get('/cart/clear', 'App\Http\Controllers\Site\CartController@clearCart')->name('checkout.cart.clear');
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('/checkout', 'Site\CheckoutController@getCheckout')->name('checkout.index');
-    Route::post('/checkout/order', 'Site\CheckoutController@placeOrder')->name('checkout.place.order');
 
-    Route::get('checkout/payment/complete', 'Site\CheckoutController@complete')->name('checkout.payment.complete');
+Route::middleware('auth')->group(function(){
+//Route::group(['middleware' => ['auth']], function () {
+    Route::get('/checkout', 'App\Http\Controllers\Site\CheckoutController@getCheckout')->name('checkout.index');
+    Route::post('/checkout/order', 'App\Http\Controllers\Site\CheckoutController@placeOrder')->name('checkout.place.order');
 
-    Route::get('account/orders', 'Site\AccountController@getOrders')->name('account.orders');
+    Route::get('checkout/payment/complete', 'App\Http\Controllers\Site\CheckoutController@complete')->name('checkout.payment.complete');
+
+    Route::get('account/orders', 'App\Http\Controllers\Site\AccountController@getOrders')->name('account.orders');
 });
 
 Auth::routes();
